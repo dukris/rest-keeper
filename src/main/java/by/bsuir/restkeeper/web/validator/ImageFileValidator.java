e3d1c8b8fd218099becb943fd1a8034740f747c8
Hanna <theannalios@gmail.com>
2023-04-03 15:45:10 +0300

    Validator for photo is added

---
+++ src/main/java/by/bsuir/restkeeper/web/validator/ImageFileValidator.java
@@ -0,0 +1,43 @@
+package by.bsuir.restkeeper.web.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.SneakyThrows;
+import org.apache.commons.compress.utils.FileNameUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ImageFileValidator implements ConstraintValidator<ValidImage, MultipartFile> {
+
+    private static final String[] TYPES = {"png", "jpg", "jpeg", "tiff"};
+
+    @Override
+    public void initialize(ValidImage constraintAnnotation) {
+
+    }
+
+    @SneakyThrows
+    @Override
+    public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext context) {
+        String extension = FileNameUtils.getExtension(multipartFile.getOriginalFilename());
+        if (multipartFile.isEmpty()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("File should not be empty!")
+                    .addConstraintViolation();
+            return false;
+        }
+        if (!isSupportedExtension(extension)) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Only " + Arrays.toString(TYPES) + " images are allowed!")
+                    .addConstraintViolation();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isSupportedExtension(String extension) {
+        return Objects.nonNull(extension) && Arrays.stream(TYPES).anyMatch(extension::equalsIgnoreCase);
+    }
+}
---
+++ src/main/java/by/bsuir/restkeeper/web/validator/ValidImage.java
@@ -0,0 +1,25 @@
+package by.bsuir.restkeeper.web.validator;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.TYPE, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = {ImageFileValidator.class})
+public @interface ValidImage {
+
+    String message() default "Invalid image file!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
