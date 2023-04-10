82c22b22cc5d65fe03b7e434b1ee4e3134ac56da
Hanna <theannalios@gmail.com>
2023-04-10 20:55:27 +0300

    Scheduler is updated

--- src/main/java/by/bsuir/restkeeper/service/scheduler/impl/MailScheduler.java
+++ src/main/java/by/bsuir/restkeeper/service/scheduler/impl/MailScheduler.java
@@ -3,17 +3,17 @@ package by.bsuir.restkeeper.service.scheduler.impl;
 import by.bsuir.restkeeper.domain.Statistics;
 import by.bsuir.restkeeper.domain.User;
 import by.bsuir.restkeeper.domain.criteria.UserSearchCriteria;
+import by.bsuir.restkeeper.service.FileService;
 import by.bsuir.restkeeper.service.MailService;
 import by.bsuir.restkeeper.service.StatisticsService;
 import by.bsuir.restkeeper.service.UserService;
-import by.bsuir.restkeeper.service.builder.impl.CsvBuilder;
+import by.bsuir.restkeeper.service.builder.Builder;
 import by.bsuir.restkeeper.service.scheduler.Scheduler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.util.List;
 
 @Slf4j
@@ -24,10 +24,11 @@ public class MailScheduler implements Scheduler {
     private final MailService mailService;
     private final StatisticsService statisticsService;
     private final UserService userService;
-    private final CsvBuilder builder;
+    private final FileService fileService;
+    private final Builder builder;
 
     @Override
-    @Scheduled(cron = "59 23 * * *")
+    @Scheduled(fixedDelay = 86400000) //24h
     public void schedule() {
         Statistics statistics = this.statisticsService.getStatistics();
         String filename = this.builder.build("report", List.of(statistics));
@@ -37,7 +38,7 @@ public class MailScheduler implements Scheduler {
                 .forEach(user -> this.mailService.send(user,
                         "statistics.ftl", "Daily Report", filename, " "));
         log.info("Daily report is sent successfully!");
-        new File(filename).delete();
+        this.fileService.delete(filename);
     }
 
 }
