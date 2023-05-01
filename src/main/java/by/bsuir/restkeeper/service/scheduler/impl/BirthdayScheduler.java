90f887241dbc86f2962c7d4d574882b87cd9c9d6
Hanna <theannalios@gmail..com>
2023-05-01 10:20:30 +0300

    Scheduler is added, resolved #20

---
+++ src/main/java/by/bsuir/restkeeper/service/scheduler/impl/BirthdayScheduler.java
@@ -0,0 +1,43 @@
+package by.bsuir.restkeeper.service.scheduler.impl;
+
+import by.bsuir.restkeeper.domain.User;
+import by.bsuir.restkeeper.domain.criteria.UserSearchCriteria;
+import by.bsuir.restkeeper.service.MailService;
+import by.bsuir.restkeeper.service.UserService;
+import by.bsuir.restkeeper.service.scheduler.Scheduler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.MonthDay;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BirthdayScheduler implements Scheduler {
+
+    private final UserService userService;
+    private final MailService mailService;
+
+    @Override
+    @Scheduled(fixedDelay = 86400000) //24h
+    public void schedule() {
+        List<User> users = this.userService.retrieveAllByCriteria(
+                new UserSearchCriteria());
+        users.forEach(user -> {
+            if (user.getDateOfBirth() != null
+                    && MonthDay.now().equals(MonthDay.from(user.getDateOfBirth()))) {
+                mailService.send(
+                        user,
+                        "birthday.ftl",
+                        "Happy Birthday!",
+                        " ",
+                        " ");
+                log.info("Congratulation is sent successfully!");
+            }
+        });
+    }
+
+}
---
+++ src/main/resources/templates/birthday.ftl
@@ -0,0 +1,8 @@
+<html lang="en">
+<body>
+<p>Dear <b>${user.name} ${user.surname}</b>!</p>
+<p>Happy birthday from Restkeeper!</p>
+<p>Our team hope all your birthday wishes and dreams come true!</p>
+<p>Have a nice day!</p>
+</body>
+</html>
