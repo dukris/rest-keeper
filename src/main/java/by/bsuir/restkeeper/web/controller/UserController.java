94fc84aa91e3d6f330cb385ddb87b5836fd0a4fc
Hanna <theannalios@gmail..com>
2023-05-08 15:58:20 +0300

    Code is refactored

--- src/main/java/by/bsuir/restkeeper/web/controller/UserController.java
+++ src/main/java/by/bsuir/restkeeper/web/controller/UserController.java
@@ -14,18 +14,10 @@ import by.bsuir.restkeeper.web.dto.mapper.criteria.UserSearchCriteriaMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -72,7 +64,7 @@ public class UserController {
     }
 
     @SneakyThrows
-    @PostMapping(value = "/{id}/photos")
+    @PostMapping(value = "/{id}/photos", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @PreAuthorize("@securityExpressions.isUser(#id) || @securityExpressions.hasAdminRole()")
     public UserDto addPhoto(@Validated ArtifactDto artifactDto,
                             @PathVariable Long id) {
